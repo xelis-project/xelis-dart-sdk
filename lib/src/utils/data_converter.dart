a2b492b7215329a4bbbee6d11f8e0cce090ed634
Ez3kiel <ez3kiel.2020@protonmail.com>
2023-03-15 12:02:45 +0100

    feat: add data converters

---
+++ lib/src/utils/data_converter.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:convert/convert.dart';
+
+/// Hash of 32 bytes as a [Uint8List].
+typedef Hash = Uint8List;
+
+/// @nodoc
+Uint8List zeroHash = Hash(32);
+
+/// Convert hexadecimal byte string to bytes array.
+Uint8List hexToBytes(String hexByteString) {
+  final bytes = hex.decode(hexByteString);
+  return Uint8List.fromList(bytes);
+}
+
+/// Convert bytes array to hexadecimal byte string.
+String bytesToHex(Uint8List bytes) {
+  final bytesList = bytes.toList();
+  return hex.encode(bytesList);
+}
+
+/// Converts a hexadecimal byte string back into its raw value.
+String hexToRawValue(String hexByteString) {
+  final bytes = hexToBytes(hexByteString);
+  return utf8.decode(bytes);
+}
+
+/// Converts a character string into a hexadecimal string.
+String stringToHex(String input) {
+  final bytes = utf8.encode(input);
+  return hex.encode(bytes);
+}
--- lib/xelis_dart_sdk.dart
+++ lib/xelis_dart_sdk.dart
@@ -5,3 +5,4 @@ export 'src/data_transfer_objects/dtos.dart';
 export 'src/repositories/daemon/daemon_channel_repository.dart';
 export 'src/repositories/daemon/daemon_client_repository.dart';
 export 'src/utils/config.dart';
+export 'src/utils/data_converter.dart';
